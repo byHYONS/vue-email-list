358d638b096515007df85075e985b3a425dd3507
byHYONS <byhyons@gmail.com>
2024-05-28 16:30:37 +0200

    [UPD] invoked function apiCall

--- main.js
+++ main.js
@@ -10,10 +10,21 @@ const {createApp} = Vue;
 const app = createApp({
     data(){
         return{
+            test: 'sono collegato',
             emails: [],
             url: 'https://flynn.boolean.careers/exercises/api/random/mail',
         }
-    }
+    },
+    methods: {
+        apiCall(){
+            axios.get(this.url).then(response => {
+                console.log(response.data.response);
+            })
+        }
+    },
+    created() {
+        this.apiCall();
+    },
 });
 
 app.mount('#app');
